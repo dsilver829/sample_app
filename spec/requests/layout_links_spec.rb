14c3f224aca807f64ae3f67f6bf0bf68cbaa1ede
David Silver <dsilver@candidatemetrics.com>
2011-01-28 16:51:02 -0500

    Finished layout and routes

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -1,23 +1,33 @@
 require 'spec_helper'
 
 describe "LayoutLinks" do
+
+  before(:each) do
+    @base_title = "Ruby on Rails Tutorial Sample App | "
+  end
+
   it "should have a Home page at '/'" do
     get '/'
-    response.should have_selector('title', :content => "Home")
+    response.should have_selector('title', :content => @base_title + "Home")
   end
 
-  it "should have a Contact page at '/'" do
-    get '/'
-    response.should have_selector('title', :content => "Contact")
+  it "should have a Contact page at '/contact'" do
+    get '/contact'
+    response.should have_selector('title', :content => @base_title + "Contact")
   end
   
-  it "should have a About page at '/'" do
-    get '/'
-    response.should have_selector('title', :content => "About")
+  it "should have a About page at '/about'" do
+    get '/about'
+    response.should have_selector('title', :content => @base_title + "About")
   end
 
-  it "should have a Help page at '/'" do
-    get '/'
-    response.should have_selector('title', :content => "Help")
+  it "should have a Help page at '/help'" do
+    get '/help'
+    response.should have_selector('title', :content => @base_title + "Help")
+  end
+
+  it "should have a signup page at /signup" do
+    get '/signup'
+    response.should have_selector("title", :content => "Sign Up")
   end
 end
