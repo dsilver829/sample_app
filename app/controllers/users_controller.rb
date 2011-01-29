0fa7f9b0347c35a4d5a7735a7c161d4cfbba55c6
David Silver <dsilver@candidatemetrics.com>
2011-01-29 12:27:23 -0500

    User signup complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
     @title = "Sign Up"
   end
 
@@ -9,4 +10,14 @@ class UsersController < ApplicationController
     @title = @user.name
   end
 
+  def create
+    @user = User.new(params[:user])
+    if(@user.save)
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      @title = 'Sign Up'
+      render 'new'
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -31,10 +31,9 @@ class User < ActiveRecord::Base
     encrypted_password == encrypt(submitted_password)
   end
 
-  def self.authenticate(email, password)
+  def self.authenticate(email, submitted_password)
     user = find_by_email(email)
-    return nil if user.nil?
-    return user if user.has_password?(password)
+    user && user.has_password?(submitted_password) ? user : nil
   end
 
   private
