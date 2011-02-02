cc101cfe0fa32723051312d1a42ba00a7fd663d4
David <dsilver@candidatemetrics.com>
2011-02-01 22:19:40 -0800

    added microposts

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -38,12 +38,7 @@ describe UsersController do
 
   describe "GET 'show'" do
     before(:each) do
-      @attr = {	:name => "My Example", 
-								:email => "my@example.com",
-								:password => "agoodpassword", 
-								:password_confirmation => "agoodpassword" }
-      post :create, :user => @attr
-			@user = User.find_by_email(@attr[:email])
+      @user = Factory(:user)
     end
 
     it "should be successful" do
@@ -69,6 +64,14 @@ describe UsersController do
     it "should have a profile image" do
       get :show, :id => @user
       response.should have_selector("h1>img", :class => "gravatar")
+    end
+
+    it "should show the user's microposts" do
+      mp1 = Factory(:micropost, :user => @user, :content => "Foo bar")
+      mp2 = Factory(:micropost, :user => @user, :content => "Baz quux")
+      get :show, :id => @user
+      response.should have_selector("span.content", :content => mp1.content)
+      response.should have_selector("span.content", :content => mp2.content)
     end
   end
 
